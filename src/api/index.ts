fa64645f960bc5468bd3c6e818189dffb964d4b0
Rupesh Chaudhari <30570616+hrupesh@users.noreply.github.com>
2022-05-21 20:16:12 +0530

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,6 @@ export const apiClient = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
   headers: {
-    // FIXME: Remove key before pushing
     'X-Api-key': '<Add-your-api-token-here>',
   },
 });
